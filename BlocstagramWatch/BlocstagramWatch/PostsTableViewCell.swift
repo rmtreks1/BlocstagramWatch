6629973ae158eb6fd1080672cd9a6e7b13b4c94b
Roshan Mahanama <rmtreks@gmail.com>
2015-05-13 11:55:46 +1000

    connected storyboard to custom tableViewCell

---
+++ BlocstagramWatch/BlocstagramWatch/PostsTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  PostsTableViewCell.swift
+//  BlocstagramWatch
+//
+//  Created by Roshan Mahanama on 13/05/2015.
+//  Copyright (c) 2015 RMTREKS. All rights reserved.
+//
+
+import UIKit
+
+class PostsTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet var postImage: UIImageView!
+    @IBOutlet var postComments: UILabel!
+    @IBOutlet var likesButton: UIView!
+    @IBOutlet var usernameLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
